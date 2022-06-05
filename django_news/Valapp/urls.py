f2d147e32c634e0f01f9ad60e8c5f43d75dc1763
Valery Tsischanka <houlett95@gmail.com>
2022-06-05 19:20:38 +0300

    Обновленние дизайна и моделей

--- django_news/Valapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('Hellopage/', views.hellopage, name='hellopage'),
-    path('YouarenotPrepared/', views.youarenotPrepared, name='youarenotPrepared'),
-]
